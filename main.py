67a69b414c6bbb2bfa8e7c222d74b31fe7ecc212
babatundeibukun <96484818+babatundeibukun@users.noreply.github.com>
2022-03-12 11:21:33 -0800

    Add files via upload
    This is my day 20 and 21 project of my #100 Days of code , I  built the popular snake game using python.

---
+++ main.py
@@ -0,0 +1,47 @@
+from turtle import Screen
+import time
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
+screen.listen()
+
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+game_on = True
+while game_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+    # detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+    # detect collision with wall
+    if snake.head.xcor() < -290 or snake.head.xcor() > 290 or snake.head.ycor() < -290 or snake.head.ycor() > 290:
+        game_on = False
+        scoreboard.reset()
+        snake.reset()
+
+    for segment in snake.segment:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_on = False
+            scoreboard.reset()
+            snake.reset()
+
+screen.exitonclick()
